5e4322cd1b8dcde58e80617d80a5585d10deb9ac
Robbie Kirkbride <robbie_kirkbride@hotmail.co.uk>
2022-11-02 12:27:13 +0000

    Alternative fizzbuzz program

---
+++ lib/fizzbuzz_test.rb
@@ -0,0 +1,13 @@
+class Integer
+  def fizzbuzz
+    if self % 3 == 0 && self % 5 == 0
+      'fizzbuzz'
+    elsif self % 3 == 0
+      'fizz'
+    elsif self % 5 == 0
+      'buzz'
+    else
+      self
+    end
+  end 
+end
